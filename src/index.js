c1e81d207a81934cb748a6ed45e60bfc80909bdd
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2013-11-04 13:16:09 -0500

    Revert de6249d..54281d4
    This rolls back to commit de6249d50dbae85a66bd15dd2ed56a7dc0a05dd5.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ var path = require('path');
 var vm = require('vm');
 var context_ = require('./context');
 
-module.exports = function (template, options) {
+module.exports = function (file, options) {
+  var baseDir = options.baseDir || (path.dirname(file) + "/")
 
   var context = context_.getContext({
     // handlebars template to compile
-    template: template,
+    template: fs.readFileSync(file, 'utf8'),
 
     // compiled handlebars template
     templatejs: null
@@ -16,8 +17,15 @@ module.exports = function (template, options) {
   // compile the handlebars template inside the vm context
   vm.runInContext('templatejs = Ember.Handlebars.precompile(template).toString()', context)
 
-  // extract the compiled template from the vm context and return it
-  return context.templatejs;
+  // extract the compiled template from the vm context and return it,
+  // adding template to Ember.TEMPLATES when it is required
+  var fileName = file.replace(baseDir, '')
+  var templateName = fileName.replace(/\.(handlebars|hbs)$/, '').replace(/\./g, '/')
+  var namedTemplateJs = 'Ember.TEMPLATES["' +
+    templateName +
+    '"] = Ember.Handlebars.template(' + context.templatejs + ');'
+
+  return namedTemplateJs;
 }
 
 module.exports.context = context_;
