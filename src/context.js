f7b627772f4f444ac417c69e66075ebb335c92ee
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2014-03-24 13:59:22 -0700

    emblem -> 0.3.12

--- src/context.js
+++ src/context.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var vm = require('vm');
 
 var HANDLEBARSJS = fs.readFileSync(__dirname + '/../vendor/handlebars-1.3.0.js', 'utf8')
-var EMBLEMJS = fs.readFileSync(__dirname + '/../vendor/emblem-0.3.11.js', 'utf8')
+var EMBLEMJS = fs.readFileSync(__dirname + '/../vendor/emblem-0.3.12.js', 'utf8')
 var EMBERJS = fs.readFileSync(__dirname + '/../vendor/ember-1.4.0.js', 'utf8')
 
 function getBaseSandbox() {
