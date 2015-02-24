abde8a69aab276de93f5284a3b5c2afbe6742aa0
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-02-24 12:37:22 -0500

    allow for repeat use of the same vm across different templates

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 Precompile Handlebars templates for Ember.js
 
 Modified for use in browserify asset pipeline
+
+## Usage:
+
+```
+precompiler = require('emblem-precompile')()
+compiledTemplate = precompiler(template);
+anotherCompiledTemplate = precompiler(template2)
+```
+
+Template should be passed in as a buffer. For ex,
+```
+template = fs.readFileSync(templateFilename)
+```
--- src/index.js
+++ src/index.js
@@ -3,21 +3,34 @@ var path = require('path');
 var vm = require('vm');
 var context_ = require('./context');
 
-module.exports = function (template, options) {
+// precompiler = require('emblem-precompile')()
+// compiledTemplate = precompiler(template);
+// anotherCompiledTemplate = precompiler(template2);
+module.exports = function (options) {
 
-  var context = context_.getContext({
-    // handlebars template to compile
-    template: template,
+  var context = context_.getContext(
+  {
+      // need Buffer
+      Buffer: Buffer,
 
-    // compiled handlebars template
-    templatejs: null
+      // handlebars template to compile
+      template: null,
+
+      // compiled handlebars template
+      templatejs: null
   });
 
-  // compile the handlebars template inside the vm context
-  vm.runInContext('templatejs = Emblem.precompile(Ember.Handlebars, template).toString()', context)
+  // return a function that can compile templates
+  return function(template) { // template is passed in as a buffer
 
-  // extract the compiled template from the vm context and return it
-  return context.templatejs;
+    // need to stringify buffer to pass it in as a string, and then parse it and make it a buffer on the other side
+    vm.runInContext("template = new Buffer(JSON.parse('" + JSON.stringify(template) + "'));", context)
+
+    vm.runInContext('templatejs = Emblem.precompile(Ember.Handlebars, template).toString()', context)
+    // extract the compiled template from the vm context and return it
+    return context.templatejs;
+  }
+
 }
 
 module.exports.context = context_;
